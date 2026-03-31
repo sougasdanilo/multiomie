adc6d3aadb30e1a8b3a7fb6b9c2d78d86f68e253
sougasdanilo@gmail.com <sougasdanilo>
2026-03-31 14:28:50 -0300

    UI

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-cffa4085'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vmju6aebcs"
+    "revision": "0.07u83423pus"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

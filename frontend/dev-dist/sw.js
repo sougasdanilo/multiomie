569eb19f392c953b3b2ce8338bb234c9b087dccb
sougasdanilo@gmail.com <sougasdanilo>
2026-03-31 16:05:03 -0300

    fine

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-cffa4085'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d1qrggona1c"
+    "revision": "0.e2hf88nundc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,4 +1,4 @@
-import pino from 'pino';
+import { pino } from 'pino';
 import { Request, Response, NextFunction } from 'express';
 import { randomUUID } from 'crypto';
 
